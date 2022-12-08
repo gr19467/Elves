1488c18cc38ca8fdc18c5bbb53c375213600d0d1
Gaby Rodriguez <gr19467@ms.wasatch.edu>
2022-12-08 08:03:58 -0700

    Homework
    -not a clue what I did

--- src/ElfCalories.java
+++ src/ElfCalories.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -19,30 +20,57 @@ public class ElfCalories {
         }
 
         //if we make it this far, we found our file, all is well.
+        System.out.println("File found.");
 
         //make a list of all the "elves"
-        ArrayList elves = new ArrayList();
+        ArrayList elves = new ArrayList(3000);
 
-        //create a pointer to the current line
-        String next = scr.nextLine();
         int i = 0;
 
         //while there is still a next line
-        while(scr.hasNext()) {
+        while (scr.hasNext()) {
             //if the current line isn't blank
-            if(!next.isEmpty()) {
-                //if the current elf has a value
-                if(elves.get(i).)
-                    //create a variable for the line's value
-                    String snackLine = elves.get(i).toString();
-                    int snackValue = Integer.parseInt(snackLine);
+            if (!Objects.equals(scr.nextLine(), "")) {
+                if (elves.isEmpty()) {
+                    //create a new elf
+                    elves.add(0, 0);
+                    System.out.println("The first elf has been created.");
+                }
+                    //if the current elf does not have a value yet
+                    if (elves.get(i).equals(0)) {
+                        //add the current number to the elf
+                        elves.add(i, scr.nextLine());
+                        System.out.println("Added first snack to elf. Value " + elves.get(i));
+                    } else {
+                        //create a variable for the elf's value
+                        String snackValue = elves.get(i).toString();
+                        int elfValue = Integer.parseInt(snackValue);
+                        System.out.println("Found elf's value. Current value: " + elfValue);
 
-                    //add the snack value to the elf's total value
-                    elves.add(i,next);
+                        //create a variable for the line's value
+                        int lineValue = Integer.parseInt(scr.nextLine());
+                        System.out.println("Found line's value. Current value: " + lineValue);
+
+                        //add the values
+                        int elfTotal = lineValue + elfValue;
+                        System.out.println("The elf's new value is " + elfTotal);
+
+                        //add the snack value to the elf's total value
+                        elves.add(i, elfTotal);
+                        System.out.println("Making sure the elf's value is changed: " + elves.get(i));
+                    }
+                //if the line is blank, increment i and move to the next line
+            }else{
+                i++;
+                scr.nextLine();
+
+                //also, create a new elf
+                elves.add(i, 0);
+                System.out.println("A new elf has been created. Their name is elf #" + i);
             }
-            //if the line is blank, make a new elf
-            //move the pointer to the next line
+
         }
 
+        System.out.println("The most calories carried are: ");
     }
 }
